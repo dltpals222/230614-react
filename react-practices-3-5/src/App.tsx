e1e2a2cd2cc77aa34215199fb7d74af9e17b38f7
leesemin <leesemin222@gmail.com>
2023-06-15 10:21:30 +0900

    저장

---
+++ react-practices-3-5/src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import {FC} from 'react'
+
+const App : FC = () => {
+  return (
+  <ul>
+    <li>첫번째 리스트</li>
+    <li>두번째 리스트</li>
+    <li>세번째 리스트</li>
+  </ul>
+  )
+}
+
+export default App
